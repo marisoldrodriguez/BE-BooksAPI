6d2193be2d0a926e3eec077650462b22bb3e97e2
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-06-01 14:23:04 -0400

    Added seed & routes to controllers, installed cors & configured it in index.js entry file,

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 // DEPENDENCIES AND CONFIGURATIONS
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 // MIDDLEWARE
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
-app.use(express.static('public'));
-// app.use(methodOverride, ('_method'));
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // MONGOOSE
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-  )
-  
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
 
 // CONTROLLERS AND ROUTES
-app.use('/books', require('./controllers/books.js'));
-
+app.use("/books", require("./controllers/books.js"));
 
 // HOME PAGE
-app.get('/', function (req, res) {
-    res.send('<h1>Hello World!</h1>')
-})
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
 
 // WILD CARD handles errors for paths that don't exist
-app.get('*', (req, res) => {
-    res.send('ERROR stub')
-})
+app.get("*", (req, res) => {
+  res.send("ERROR stub");
+});
 
 // Listens for connections - keeps server open
 app.listen(process.env.PORT, function () {
-    console.log('Hello, I am ready!')
-})
+  console.log("Hello, I am ready!");
+});
